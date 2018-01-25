f98b946d4d442c88ac479de1371ac873200f1234
Gigi Frias <gigifrias@users.noreply.github.com>
2018-01-25 14:21:02 -0500

    Updating JS

--- js/index.js
+++ js/index.js
@@ -3,21 +3,21 @@ $(document).ready ( function() {
 	*** uses countup.js
 	**/
 	// find all elements with counter class
-	var targetList = document.querySelectorAll( '.counter' );
+	var counterList = document.querySelectorAll( '.counter' );
 
 	// loop through all target objects in array
-	jQuery.each( targetList, function( i, val ) {
+	jQuery.each( counterList, function( i, val ) {
         // get target element id
-        targetID = val.id;
-        // console.log(targetID);
+        counterID = val.id;
+        // console.log(counterID);
         // get target element counter parameters
-		var params = document.getElementById( targetID );
+		var cParams = document.getElementById( counterID );
 
 		// declare the function for the count
-	    var animatedNumb = new CountUp( targetID , params.dataset.startval, params.dataset.endval, 0, params.dataset.duration );
+	    var animatedNumb = new CountUp( counterID , cParams.dataset.startval, cParams.dataset.endval, 0, cParams.dataset.duration );
 	    //execute the function and output message to console
 	    if ( !animatedNumb.error ) { // function ok
-	        animatedNumb.start( /* console.log( "the number has been logged for " + targetID )*/ );
+	        animatedNumb.start( /* console.log( "the number has been logged for " + counterID )*/ );
 	    } else { // function error
 	        console.error( animatedNumb.error );
 	        // console.log( isNaN(settings.startVal) );
@@ -26,26 +26,55 @@ $(document).ready ( function() {
 
     // init ScrollMagic controller
 	var controller = new ScrollMagic.Controller({
-		loglevel: 0, // console log
-		globalSceneOptions: {duration: 1000} // default scene settings
+		loglevel: 3, // console log
+		globalSceneOptions: { // default scene settings
+			triggerHook: 'onCenter',
+			offset: 20,
+			reverse: true
+		} 
 	});
 
-	// set tween for logos
-	// var logo = document.getElementById( "facebook" );
-	// var tween = TweenLite.from( logo, 0.75, {scale: 0.2, opacity: 0.3 } );
+	// find all elements with counter class
+	var sectionList = document.querySelectorAll( '.section' );
 
-	// build ScrollMagic scenes
-	var scene = new ScrollMagic.Scene({
-			triggerElement: "#section-1",
-			triggerHook: 'onLeave',
-			// offset: 200,
-			// duration: ,
-			loglevel: 2
-		})
-		.setPin("#facebook")
-		.setClassToggle( $( '#facebook.brand' ), "show" ) // add class toggle
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controller);
+	// loop through all target objects in array
+	jQuery.each( sectionList, function( s, val ) {
+        // get target element id
+        sectionID = val.id;
+        // console.log(sectionID);
+        // get target element counter parameters
+		var sParams = document.getElementById( sectionID );
+		var index = $(sParams).index();
+		var pin = '#' + sectionID;
+		var b = '#' + $(sParams).data("brand");
+		var t = $(sParams).eq(index - 1 * index).offset().top;
+		var h = $(sParams).height();
+
+		/*if ( $(sParams).height() > $(window).height() ) {
+			h = $(sParams).height();
+		} else {
+			h = 0;
+			// $(sParams).css({display:"none"});
+		}*/
+		console.log ( "INDEX: " + index +  " PIN: " + pin + " brand: " + b + " offset: " + t + " height: " + h );
+		// declare the function for the scroll trigger
+		if ( t < $(window).scrollTop() < h ) {
+			var sectionID = new ScrollMagic.Scene({
+				triggerElement: b,
+				duration: h,
+				// offset: 0
+				offset: t + 60
+			})
+			.setPin( b, {
+				pushFollowers: true
+			})		
+			// .setClassToggle( b, "show" ) // add class toggle
+			.addIndicators() // add trigger indicators (requires plugin)
+			.addTo(controller);
+		} else {
+			controller.removeScene(sectionID);
+		}
+    });
 });
 
 /* Parallax function */ // requires jquery
