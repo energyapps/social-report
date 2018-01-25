02945b5253f39503d4a68ea7dc7a70282a1f41f6
Gigi Frias <gigi.frias@gmail.com>
2018-01-25 16:55:31 -0500

    Updates to the scrollmagic actions. Trying to troubleshoot extra space between sections.

--- js/index.js
+++ js/index.js
@@ -1,29 +1,4 @@
 $(document).ready ( function() {
-	/** COUNTERS: IMPRESSIONS
-	*** uses countup.js
-	**/
-	// find all elements with counter class
-	var counterList = document.querySelectorAll( '.counter' );
-
-	// loop through all target objects in array
-	jQuery.each( counterList, function( i, val ) {
-        // get target element id
-        counterID = val.id;
-        // console.log(counterID);
-        // get target element counter parameters
-		var cParams = document.getElementById( counterID );
-
-		// declare the function for the count
-	    var animatedNumb = new CountUp( counterID , cParams.dataset.startval, cParams.dataset.endval, 0, cParams.dataset.duration );
-	    //execute the function and output message to console
-	    if ( !animatedNumb.error ) { // function ok
-	        animatedNumb.start( /* console.log( "the number has been logged for " + counterID )*/ );
-	    } else { // function error
-	        console.error( animatedNumb.error );
-	        // console.log( isNaN(settings.startVal) );
-	    }
-    });
-
     // init ScrollMagic controller
 	var controller = new ScrollMagic.Controller({
 		loglevel: 3, // console log
@@ -35,9 +10,11 @@ $(document).ready ( function() {
 	});
 
 	// find all elements with counter class
+	var counterList = document.querySelectorAll( '.counter' );
+	// find each social platform section
 	var sectionList = document.querySelectorAll( '.section' );
 
-	// loop through all target objects in array
+	// loop through all the sections
 	jQuery.each( sectionList, function( s, val ) {
         // get target element id
         sectionID = val.id;
@@ -50,30 +27,45 @@ $(document).ready ( function() {
 		var t = $(sParams).eq(index - 1 * index).offset().top;
 		var h = $(sParams).height();
 
-		/*if ( $(sParams).height() > $(window).height() ) {
-			h = $(sParams).height();
-		} else {
-			h = 0;
-			// $(sParams).css({display:"none"});
-		}*/
-		console.log ( "INDEX: " + index +  " PIN: " + pin + " brand: " + b + " offset: " + t + " height: " + h );
+		// console.log ( "INDEX: " + index +  " PIN: " + pin + " TRIGGER: " + b + " OFFSET: " + t + " HEIGHT: " + h );
+		// console.log ( controller.info() );
 		// declare the function for the scroll trigger
-		if ( t < $(window).scrollTop() < h + $(window).scrollTop() ) {
-			var sectionID = new ScrollMagic.Scene({
+		// if ( t < $(window).scrollTop() < h + $(window).scrollTop() ) {
+			// create scroll scene
+			var sectionScene = new ScrollMagic.Scene({
 				triggerElement: b,
 				duration: h,
 				// offset: 0
 				offset: h + 60
 			})
 			.setPin( b, {
-				pushFollowers: true
+				pushFollowers: false
 			})		
 			.setClassToggle( b, "show" ) // add class toggle
 			.addIndicators() // add trigger indicators (requires plugin)
 			.addTo(controller);
-		} else {
-			controller.removeScene(sectionID);
-		}
+
+			// loop through all counters
+			jQuery.each( counterList, function( i, val ) {
+		        // get target element id
+		        counterID = val.id;
+		        // console.log(counterID);
+		        // get target element counter parameters
+				var cParams = document.getElementById( counterID );
+
+				// declare the function for the count
+			    var animatedNumb = new CountUp( counterID , cParams.dataset.startval, cParams.dataset.endval, 0, cParams.dataset.duration );
+			    //execute the function and output message to console
+			    if ( !animatedNumb.error ) { // function ok
+			        animatedNumb.start( /*console.log( "the number has been logged for " + counterID )*/ );
+			    } else { // function error
+			        console.error( animatedNumb.error );
+			        // console.log( isNaN(settings.startVal) );
+			    }
+		    });
+		// } else {
+		// 	controller.removeScene(sectionScene);
+		// }
     });
 });
 
